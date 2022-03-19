406214c57bc8012f93bf464f327d17bef9e4eea7
Laura Smith <laurajayne8@live.com>
2022-03-19 17:08:37 +0000

    creating server page

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from "express"
+import router from "./views/router.js"
+import logger from "./middleware/logger.js"
+import errorHandler from "./middleware/errorHandler.js"
+
+async function startServer() {
+  const app = express();
+
+//   dotenv.config();
+  const PORT = process.env.PORT ? process.env.PORT : 4000;
+
+// setting up express to use
+  app.use(express.json());
+
+  app.use(logger);
+  app.use(router);
+  app.use(errorHandler);
+
+  await connectToDb();
+  console.log("Claudia's message to you! ");
+  app.listen(PORT, () => console.log(`Express server running on Port ${PORT}`));
+}
+startServer();
